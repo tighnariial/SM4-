df9c6603d2f73bff2bb9df27a200eafecf4277d7
tighnariial <151018748+tighnariial@users.noreply.github.com>
2025-08-14 14:01:48 +0800

    Update sm4.cpp

--- sm4.cpp
+++ sm4.cpp
@@ -39,12 +39,12 @@ uint32_t Sbox(uint32_t x) {
     return ((SBOX[x >> 24] & 0xff) << 24 | (SBOX[x >> 16] & 0xff) << 16 | (SBOX[x >> 8] & 0xff) << 8 | (SBOX[x] & 0xff));
 }
 
-//���ܹ��̵�L�任
+//加密过程的L变换
 uint32_t L(uint32_t x) {
     return x ^ (x << 2 | x >> (32 - 2)) ^ (x << 10 | x >> (32 - 10)) ^ (x << 18 | x >> (32 - 18)) ^ (x << 24 | x >> (32 - 24));
 }
 
-//��Կ���ɷ�����L�任
+//密钥生成方案的L变换
 uint32_t LK(uint32_t x) {
     return x ^ (x << 13 | x >> (32 - 13)) ^ (x << 23 | x >> (32 - 23));
 }
@@ -62,26 +62,37 @@ void sm4_key_schedule(const uint32_t MK[4], uint32_t rk[32]) {
     }
 }
 
-__m128i SM4_round(__m128i x0,__m128i x1, __m128i x2, __m128i x3, uint32_t rk) {
+//生成预计算T-table
+void generate_SM4_Ttable(uint32_t T0[256], uint32_t T1[256], uint32_t T2[256], uint32_t T3[256]) {
+    for (int i = 0; i < 256; i++) {
+        uint32_t s = SBOX[i];
+        uint32_t val = L((s << 24) | (s << 16) | (s << 8) | s); 
+        T0[i] = val;
+        T1[i] = (val << 8 | val >> 24);
+        T2[i] = (val << 16 | val >> 16);
+        T3[i] = (val << 24 | val >> 8);
+    }
+}
+
+//使用T-table优化轮函数，避免线性运算周期
+__m128i SM4_round(__m128i x0,__m128i x1, __m128i x2, __m128i x3, uint32_t rk,const uint32_t T0[256], const uint32_t T1[256],const uint32_t T2[256], const uint32_t T3[256]) {
     __m128i tmp = _mm_xor_si128(_mm_xor_si128(x1, x2), _mm_xor_si128(x3, _mm_set1_epi32(rk)));
     alignas(16) uint32_t arr[4];
     _mm_store_si128((__m128i*)arr,tmp );
-    for (int i = 0; i < 4; i++) {
-        arr[i] = L(Sbox(arr[i]));
-    }
-    __m128i res = _mm_load_si128((__m128i*)arr);
-    return(_mm_xor_si128(x0, res));
-}
+    uint32_t t = T0[(arr[0] >> 24) & 0xFF] ^ T1[(arr[1] >> 16) & 0xFF] ^
+        T2[(arr[2] >> 8) & 0xFF] ^ T3[arr[3] & 0xFF];
+    return _mm_xor_si128(x0, _mm_set1_epi32(t));
 
 
-void SM4_ENC_SIMD_4(const uint32_t IN[4][4],uint32_t OUT[4][4], const uint32_t rk[32]) {
+
+void SM4_ENC_SIMD_4(const uint32_t IN[4][4],uint32_t OUT[4][4], const uint32_t rk[32], const uint32_t T0[256], const uint32_t T1[256], const uint32_t T2[256], const uint32_t T3[256]) {
     __m128i x0 = _mm_set_epi32(IN[3][0], IN[2][0], IN[1][0], IN[0][0]);
     __m128i x1 = _mm_set_epi32(IN[3][1], IN[2][1], IN[1][1], IN[0][1]);
     __m128i x2 = _mm_set_epi32(IN[3][2], IN[2][2], IN[1][2], IN[0][2]);
     __m128i x3 = _mm_set_epi32(IN[3][3], IN[2][3], IN[1][3], IN[0][3]);
 
     for (int round = 0; round < 32; round++) {
-        __m128i tmp = SM4_round(x0, x1, x2, x3, rk[round]);
+        __m128i tmp = SM4_round(x0, x1, x2, x3, rk[round], T0, T1, T2, T3);
         x0 = x1; x1 = x2; x2 = x3; x3 = tmp;
     }
 
